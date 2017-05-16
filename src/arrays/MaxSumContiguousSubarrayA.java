4c007d85c95d86c94e7eba5f0ddc1709375c8af7
Ilya Ismagilov <ismagilov@gmail.com>
2017-05-16 17:26:49 +0700

    Solve using max slice for current element

---
+++ src/arrays/MaxSumContiguousSubarrayA.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class MaxSumContiguousSubarrayA {
+	// DO NOT MODFIY THE LIST. 
+	public int maxSubArray(final List<Integer> a) {
+	    if (a.size() == 0)
+	        return -1000000000;
+	        
+	    int max = a.get(0);
+	    int slice = a.get(0);
+	    
+	    for (int i = 1; i < a.size(); i++) {
+	        slice = Math.max(slice + a.get(i), a.get(i));
+	        max = Math.max(max, slice);
+	    }
+	    
+	    return max;
+	}
+}
