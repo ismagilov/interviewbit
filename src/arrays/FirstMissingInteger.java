308263840a12995482378d6d5f0b5e09ef6bc982
Ilya Ismagilov <ilya@singulator.net>
2017-05-17 20:48:50 +0700

    Simplified a solution

--- src/arrays/FirstMissingInteger.java
+++ src/arrays/FirstMissingInteger.java
@@ -7,31 +7,27 @@ import java.util.ArrayList;
 */
 public class FirstMissingInteger {
 	public int firstMissingPositive(ArrayList<Integer> a) {
-	    Integer missing = 1;
-	    
-	    for (int i = 0; i < a.size(); i++) {
-	        int idx = i;
-	        int val = a.get(idx);
-            
-            //1 <= val && val <= a.size() - we need to count
-            //val - 1 != idx - value is not on its place
-            //a.get(val - 1) != val - index to where we can move already has correct number
-	        while (1 <= val && val <= a.size() && val - 1 != idx && a.get(val - 1) != val) {
-	            int moveToIdx = val - 1;
-	            
-	            int tmp = a.get(moveToIdx);
-	            a.set(moveToIdx, val);
-	            a.set(idx, tmp);
-	            
-	            val = tmp;
-	        }
-	    }
+		Integer missing = 1;
 
-	    for (int i = 0; i < a.size(); i++) {
-	        if (i + 1 != a.get(i))
-	            return i + 1; 
-	    }
+		for (int i = 0; i < a.size(); i++) {
+			if (1 <= a.get(i) && a.get(i) <= a.size()) {
+				int pos = a.get(i) - 1;
 
-	    return a.size() + 1;
+				if (a.get(pos) != a.get(i)) {
+					int tmp = a.get(pos);
+					a.set(pos, a.get(i));
+					a.set(i, tmp);
+
+					i--;
+				}
+			}
+		}
+
+		for (int i = 0; i < a.size(); i++) {
+			if (i + 1 != a.get(i))
+				return i + 1;
+		}
+
+		return a.size() + 1;
 	}
 }
