b5a689da4e4cbac8e74088b8b5d980b287980121
Ilya Ismagilov <ismagilov@gmail.com>
2017-05-17 15:09:24 +0700

    Solved using O(N + M) space

---
+++ src/arrays/SetMatrixZeros.java
@@ -0,0 +1,28 @@
+package arrays;
+
+/**
+* https://www.interviewbit.com/problems/set-matrix-zeros/
+*/
+public class SetMatrixZeros {
+	public void setZeroes(ArrayList<ArrayList<Integer>> a) {
+	    LinkedHashSet<Integer> rows = new LinkedHashSet<>();
+	    LinkedHashSet<Integer> columns = new LinkedHashSet<>();
+	    
+	    for (int i = 0; i < a.size(); i++) {
+	        for (int j = 0; j < a.get(i).size(); j++) {
+	            if (a.get(i).get(j) == 0) {
+	                rows.add(i);
+	                columns.add(j);
+	            }
+	        }
+	    }
+	    
+	    for (Integer r : rows)
+	        for (int j = 0; j < a.get(r).size(); j++)
+	            a.get(r).set(j, 0);
+	    
+	    for (int i = 0; i < a.size(); i++)
+	        for (Integer c : columns)
+	            a.get(i).set(c, 0);
+	}
+}
