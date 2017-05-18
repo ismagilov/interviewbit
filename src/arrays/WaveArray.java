a9c11a348702be01e34284c4e3c228dbb45b9257
Ilya Ismagilov <ismagilov@gmail.com>
2017-05-18 10:17:40 +0700

    Solved

---
+++ src/arrays/WaveArray.java
@@ -0,0 +1,19 @@
+package arrays;
+
+/**
+* https://www.interviewbit.com/problems/wave-array/
+*/
+public class WaveArray {
+	public ArrayList<Integer> wave(ArrayList<Integer> a) {
+	    a.sort(Comparator.naturalOrder());
+	    
+	    int b = 0, e = a.size() - 1;
+	    for (int i = 0; i + 1 < a.size(); i += 2) {
+	        int tmp = a.get(i + 1);
+	        a.set(i + 1, a.get(i));
+	        a.set(i, tmp);
+	    }
+	   
+	    return a;     
+	}
+}
