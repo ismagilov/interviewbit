b23c973e34f7d8eae0a0d8994bb35a93e4da2d88
Ilya Ismagilov <ismagilov@gmail.com>
2017-05-18 11:21:15 +0700

    Solved using O(N) space

---
+++ src/arrays/FindDuplicateInArrayA.java
@@ -0,0 +1,22 @@
+package arrays;
+
+/**
+* https://www.interviewbit.com/problems/find-duplicate-in-array/
+* 
+* Space O(n)
+*/
+public class FindDuplicateInArrayA {
+	// DO NOT MODIFY THE LIST
+	public int repeatedNumber(final List<Integer> a) {
+	    int[] count = new int[a.size()];
+	    
+	    for (int i = 0; i < a.size(); i++)
+	        count[a.get(i)]++;
+	    
+	    for (int i = 0; i < count.length; i++)
+	        if (count[i] > 1)
+	            return i;
+	        
+	    return -1;
+	}
+}
