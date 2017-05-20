9fc7a2e106e6c8317a3e7c6f6bc7cc96bd440fb0
Ilya Ismagilov <ismagilov@gmail.com>
2017-05-20 21:19:52 +0700

    Solved

---
+++ src/arrays/RotateMatrix.java
@@ -0,0 +1,21 @@
+package arrays;
+
+/**
+* https://www.interviewbit.com/problems/rotate-matrix/
+*/
+public class RotateMatrix {
+	public void rotate(ArrayList<ArrayList<Integer>> a) {
+	    int n = a.size();
+	    
+	    for (int i = 0; i < n/2; i++) {
+	        for (int j = i; j < n - 1 - i; j++) {
+	            int tmp = a.get(i).get(j);
+	            
+	            a.get(i).set(j, a.get(n - 1 - j).get(i));
+	            a.get(n - 1 - j).set(i, a.get(n - 1 - i).get(n - 1 - j));
+	            a.get(n - 1 - i).set(n - 1 - j, a.get(j).get(n - 1 - i));
+	            a.get(j).set(n - 1 - i, tmp);
+	        }
+	    }
+	}
+}
