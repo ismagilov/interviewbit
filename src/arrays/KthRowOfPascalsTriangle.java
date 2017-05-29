83e61fb32aea48fc2299b7d714c05cb1804c8447
Ilya Ismagilov <ismagilov@gmail.com>
2017-05-29 15:55:15 +0700

    Solved

---
+++ src/arrays/KthRowOfPascalsTriangle.java
@@ -0,0 +1,28 @@
+package arrays;
+
+public class KthRowOfPascalsTriangle {
+	public ArrayList<Integer> getRow(int a) {
+	    ArrayList<Integer> res = new ArrayList<>();
+	    
+	    res.add(1);
+	    if (a == 0)
+	        return res;
+	    
+	    for (int i = 1; i <= a; i++) {
+	        ArrayList<Integer> prev = res;
+	        res = new ArrayList<Integer>();
+	        
+	        res.add(1);
+	        
+	        int idx = 1;
+	        while (idx < prev.size()) {
+	            res.add(prev.get(idx) + prev.get(idx - 1));
+	            idx++;
+	        }
+	        
+	        res.add(1);
+	    }
+	    
+	    return res;
+	}
+}
