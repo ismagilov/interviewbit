9e809f0483799b40c95be03962b6358bc190c004
Ilya Ismagilov <ismagilov@gmail.com>
2017-05-29 13:45:55 +0700

    Solved

---
+++ src/arrays/PascalTriangleRowsA.java
@@ -0,0 +1,31 @@
+package arrays;
+
+/**
+* https://www.interviewbit.com/problems/pascal-triangle-rows/
+*/
+public class Solution {
+	public ArrayList<ArrayList<Integer>> generate(int a) {
+	    ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+	    ArrayList<Integer> row = new ArrayList<>();
+	    row.add(1);
+	    
+	    if (a > 0)
+	        res.add(row);
+	    
+	    for (int i = 1; i < a; i ++) {
+	        ArrayList<Integer> prev = res.get(i - 1);
+	        ArrayList<Integer> cur = new ArrayList<>();
+	        for (int j = 0; j <= prev.size(); j++) {
+	            if (j == 0 || j == prev.size())
+	                cur.add(1);
+	            else {
+	                cur.add(prev.get(j) + prev.get(j - 1));
+	            }
+	        }
+	        
+	        res.add(cur);
+	    }
+	    
+	    return res;   
+	}
+}
