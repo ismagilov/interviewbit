1abbda7fe95950e2d6d5c5488e7c3048a32248c0
Ilya Ismagilov <ismagilov@gmail.com>
2017-05-31 12:09:15 +0700

    Solved

---
+++ src/math/PrimeNumbersSieve.java
@@ -0,0 +1,29 @@
+package math;
+
+/**
+* https://www.interviewbit.com/problems/prime-numbers/
+*/
+public class PrimeNumbersSieve {
+	public ArrayList<Integer> sieve(int a) {
+	    int[] primes = new int[a + 1];
+	    for (int i = 0; i <= a; i++)
+	        primes[i] = 1;
+	    
+	    primes[0] = 0;
+	    primes[1] = 0;
+	    
+	    for (int i = 2; i <= Math.sqrt(a); i++) {
+	        if (primes[i] == 1) {
+	            for (int j = i; i * j <= a; j++)
+	                primes[i * j] = 0;
+	        }
+	    }
+	    
+	    ArrayList<Integer> res = new ArrayList<>();
+	    for (int i = 0; i <= a; i++) 
+	        if (primes[i] == 1)
+	            res.add(i);
+	            
+	    return res;
+	}
+}
