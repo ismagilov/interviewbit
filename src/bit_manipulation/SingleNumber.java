99440a089a1b659f3120b28bad615352b72ae11a
Ilya Ismagilov <ismagilov@gmail.com>
2017-06-02 11:19:05 +0700

    Solved

---
+++ src/bit_manipulation/SingleNumber.java
@@ -0,0 +1,16 @@
+/**
+* https://www.interviewbit.com/problems/single-number/
+*/
+public class SingleNumber {
+	// DO NOT MODIFY THE LIST
+	public int singleNumber(final List<Integer> a) {
+	    int res = 0;
+	    
+	    for (int elem : a) {
+	        res = res ^ elem;
+	    }
+	    
+	    return res;
+	}
+}
+
