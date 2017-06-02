8b20854eebd367b3ceaa5081093166b0d749d78c
Ilya Ismagilov <ismagilov@gmail.com>
2017-06-02 12:21:34 +0700

    Solved: XOR can be min only for adjacent elements

---
+++ src/bit_manipulation/MinXorValue.java
@@ -0,0 +1,16 @@
+package bit_manipulation;
+
+/**
+* https://www.interviewbit.com/problems/min-xor-value/
+*/
+public class MinXorValue {
+    public int findMinXor(ArrayList<Integer> A) {
+        A.sort(Comparator.naturalOrder());
+        
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < A.size() - 1; i++)
+            min = Math.min(min, A.get(i) ^ A.get(i + 1));
+        
+        return min;
+    }
+}
