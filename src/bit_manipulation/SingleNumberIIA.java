c0d73750ee1e08b66f9d2721caa7a428c02a5f36
Ilya Ismagilov <ismagilov@gmail.com>
2017-06-05 10:58:30 +0700

    Solved using int[32]

---
+++ src/bit_manipulation/SingleNumberIIA.java
@@ -0,0 +1,31 @@
+package bit_manipulation;
+
+/**
+* https://www.interviewbit.com/problems/single-number-ii/
+*/
+public class SingleNumberII {
+	// DO NOT MODIFY THE LIST
+	public int singleNumber(final List<Integer> a) {
+	    int[] bitsSum = new int[32];
+	    
+	    for (Integer e : a) {
+	        long l = (long)e;
+	        
+	        for (int i = 0; i < 32; i++) {
+	            bitsSum[i] += l & 1; 
+	            l >>= 1;
+	        }
+	            
+	    }
+	    
+        //System.out.println(Arrays.toString(bitsSum));
+        
+        long res = 0;
+        for (int i = 31; i >= 0; i--) {
+            res <<= 1;
+            res |= bitsSum[i] % 3;
+        }
+	    
+	    return (int)res;
+	}
+}
