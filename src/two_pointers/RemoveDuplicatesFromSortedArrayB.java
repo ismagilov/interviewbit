cad8c312632a9fc73480970772ab0144c9cbe3fe
Ilya Ismagilov <ismagilov@gmail.com>
2017-06-06 11:58:31 +0700

    Solved by moving unique elements into beginning

---
+++ src/two_pointers/RemoveDuplicatesFromSortedArrayB.java
@@ -0,0 +1,19 @@
+package two_pointers;
+
+/**
+* https://www.interviewbit.com/problems/remove-duplicates-from-sorted-array/
+*/
+public class RemoveDuplicatesFromSortedArrayB {
+	public int removeDuplicates(ArrayList<Integer> a) {
+        int index = 1;
+        
+        for (int i = 1; i < a.size(); i++) {
+            if (a.get(i).intValue() != a.get(i - 1).intValue()) {
+                a.set(index, a.get(i));
+                index++;
+            }    
+        }
+	   
+	   return index;
+	}
+}
