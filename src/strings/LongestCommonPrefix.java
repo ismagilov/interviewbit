cbdee2d761c0490d72a4f01ae24c175149ba1029
Ilya Ismagilov <ismagilov@gmail.com>
2017-06-07 10:27:44 +0700

    Reformatted indents

--- src/strings/LongestCommonPrefix.java
+++ src/strings/LongestCommonPrefix.java
@@ -6,25 +6,25 @@ package strings;
 public class LongestCommonPrefix {
 	public String longestCommonPrefix(ArrayList<String> a) {
 	    if (a.size() == 0)
-	        return "";
-	    
+		return "";
+
 	    String base = a.get(0);
-	    
+
 	    int i = 0;
 	    boolean found = false;
 	    for (i = 0; i < base.length(); i++) {
-	        for (int j = 1; j < a.size(); j++) {
-	            if (i >= a.get(j).length() || a.get(j).charAt(i) != base.charAt(i)) {
-	                found = true;
-	                break;
-	            }
-	        }
-	        
-	        if (found)
-	            break;
+		for (int j = 1; j < a.size(); j++) {
+		    if (i >= a.get(j).length() || a.get(j).charAt(i) != base.charAt(i)) {
+			found = true;
+			break;
+		    }
+		}
+
+		if (found)
+		    break;
 	    }
 
-        return base.substring(0, i);
+		return base.substring(0, i);
 	}
 }
 
