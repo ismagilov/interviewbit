7c4c6af89085d21a921b700b2da8d35905a045ff
Ilya Ismagilov <ismagilov@gmail.com>
2017-06-19 12:39:46 +0700

    Solved using b<e and A[mid] < x approach

---
+++ src/binary_search/SearchForARangeB.java
@@ -0,0 +1,44 @@
+package binary_search;
+
+/**
+* https://www.interviewbit.com/problems/search-for-a-range/
+*/
+public class SearchForARangeB {
+	// DO NOT MODIFY THE LIST
+	public ArrayList<Integer> searchRange(final List<Integer> arr, int x) {
+	    int b = 0, e = arr.size() - 1;
+	    
+	    ArrayList<Integer> res = new ArrayList<>();
+	    res.add(-1);
+	    res.add(-1);
+
+	    while (b < e) {
+	        int mid = b + (e - b) / 2;
+
+	        if (arr.get(mid) < x)
+	            b = mid + 1;
+	        else
+	            e = mid;
+	    }
+	    
+	    if (arr.get(b) != x)
+            return res;
+        else
+            res.set(0, b);
+	    
+	    e = arr.size() - 1;
+	    while (b < e) {
+	        int mid = b + (e - b) / 2 + 1; // Move a middle to the right
+
+            if (arr.get(mid) > x)
+	            e = mid - 1;
+	        else
+	            b = mid;
+	    }
+	    
+        res.set(1, e);
+
+	    return res;
+	}
+
+}
