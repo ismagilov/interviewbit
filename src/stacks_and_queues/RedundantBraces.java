555715236a94288d9a682f614ebdc51a30203f12
Ilya Ismagilov <ismagilov@gmail.com>
2017-07-10 12:40:10 +0700

    Solved by removing subexpressions from a stack

---
+++ src/stacks_and_queues/RedundantBraces.java
@@ -0,0 +1,42 @@
+package stacks_and_queues;
+
+/**
+* https://www.interviewbit.com/problems/redundant-braces/
+*/ 
+public class RedundantBraces {
+    private Stack<Character> stack = new Stack<>();
+    
+    private ArrayList<Character> ops = new ArrayList<>();
+    private ArrayList<Character> braces = new ArrayList<>();
+    
+	public int braces(String a) {
+	    braces.add('(');
+	    braces.add(')');
+	    ops.add('+');
+	    ops.add('-');
+	    ops.add('*');
+	    ops.add('/');
+	    
+	    char[] syms = a.toCharArray();
+	    for (int i = 0; i < syms.length; i++) {
+	        char c = syms[i];
+	        if (!braces.contains(c) && !ops.contains(c))
+	            continue;
+
+	        if (c == ')') {
+	            if (ops.contains(stack.peek())) {
+	                stack.pop();
+	                
+	                if (stack.peek() == '(')
+	                    stack.pop();
+	            } else if (stack.peek() == '(') {
+	                return 1;
+	            }
+	        } else {
+	            stack.push(c);
+	        }
+	    }
+	    
+	    return 0;
+	}
+}
