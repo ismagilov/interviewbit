5c8ba00b32bda4e80050c12b67ab2c681aa76c51
Ilya Ismagilov <ismagilov@gmail.com>
2017-07-10 11:40:57 +0700

    Solved using the second stack for current min

---
+++ src/stacks_and_queues/MinStackB.java
@@ -0,0 +1,41 @@
+package stacks_and_queues;
+
+/**
+* https://www.interviewbit.com/problems/min-stack/
+*
+* Use the second stack for saving current min.
+*/
+class MinStackB {
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> mins = new Stack<>();
+    
+    public void push(int x) {
+        if (stack.empty() || x < mins.peek())
+            mins.push(x);
+        
+        stack.push(x);
+    }
+
+    public void pop() {
+        if (!stack.empty()) {
+            int v = stack.pop();
+            
+            if (v == mins.peek())
+                mins.pop();
+        }
+    }
+
+    public int top() {
+        if (stack.empty())
+            return -1;
+        else 
+            return stack.peek();
+    }
+
+    public int getMin() {
+        if (mins.empty())
+            return -1;
+        else
+            return mins.peek();
+    }
+}
