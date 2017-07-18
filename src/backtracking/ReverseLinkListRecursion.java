fa51b4e0e4521e973d30628189c904e4912f1960
Ilya Ismagilov <ismagilov@gmail.com>
2017-07-18 12:35:43 +0700

    Solved

---
+++ src/backtracking/ReverseLinkListRecursion.java
@@ -0,0 +1,25 @@
+package backtracking;
+
+/**
+ * https://www.interviewbit.com/problems/reverse-link-list-recursion/
+ *
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class ReverseLinkListRecursion {
+	public ListNode reverseList(ListNode a) {
+	    if (a == null || a.next == null)
+	        return a;
+	        
+	    ListNode res = reverseList(a.next);
+	    
+	    a.next.next = a;
+	    a.next = null;
+	    
+        return res;	    
+	}
+}
