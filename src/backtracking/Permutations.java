3ac4d728a157663467f0c7cc0bc3767c032ebf73
Ilya Ismagilov <ismagilov@gmail.com>
2017-07-27 12:06:12 +0700

    Solved

---
+++ src/backtracking/Permutations.java
@@ -0,0 +1,35 @@
+/**
+* https://www.interviewbit.com/problems/permutations/
+*/
+public class Permutations {
+	public ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> a) {
+	    Collections.sort(a);
+	    
+	    return generate(a, 0);
+	}
+	
+	private  ArrayList<ArrayList<Integer>> generate(ArrayList<Integer> digits, int start) {
+	    ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+	    
+	    if (start == digits.size() - 1) {
+	        res.add(new ArrayList<>(digits));
+	        return res;
+	    }
+	    
+	    for (int i = start; i < digits.size(); i++) {
+           digits = swap(digits, start, i);
+	       res.addAll(generate(digits, start + 1));
+           digits = swap(digits, start, i);
+	    } 
+	    
+	    return res;
+	}
+	
+	private ArrayList<Integer> swap(ArrayList<Integer> arr, int i, int j) {
+	    int temp = arr.get(i);
+	    arr.set(i, arr.get(j));
+	    arr.set(j , temp);
+	    
+	    return arr;
+	}
+}
