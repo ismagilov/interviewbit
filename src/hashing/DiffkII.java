19f8de1b9e79ba869af5eb757b891b2f8c319e23
Ilya Ismagilov <ismagilov@gmail.com>
2017-07-31 17:05:14 +0700

    Solved

---
+++ src/hashing/DiffkII.java
@@ -0,0 +1,23 @@
+package: hashing;
+
+/**
+* https://www.interviewbit.com/problems/diffk-ii/
+*/
+public class DiffkII {
+	public int diffPossible(final List<Integer> arr, int diff) {
+	    HashSet<Integer> set = new HashSet<>();
+	    
+	    for (Integer n : arr) {
+	        // A[i] - A[j] = k, i != j, so i can be greater than j and vice versa
+	        // While we iterate through the array, we need to check that 
+	        // set contains current plus diff (A[i] - A[cur] = diff) or
+	        // set contains current minus diff (A[cur] - A[i] = diff) 
+	        if (set.contains(n + diff) || h.contains(n - diff)) 
+	            return 1;
+	        
+	        set.add(n);
+	    }
+	    
+	    return 0;
+	}
+}
