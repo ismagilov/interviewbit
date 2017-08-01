581be5d576fdbe32f4ef9c99b017ac190a479b4f
Ilya Ismagilov <ismagilov@gmail.com>
2017-08-01 11:35:58 +0700

    Solved

---
+++ src/hashing/Anagrams.java
@@ -0,0 +1,29 @@
+/**
+* https://www.interviewbit.com/problems/anagrams/
+*/
+public class Anagrams {
+	public ArrayList<ArrayList<Integer>> anagrams(final List<String> ws) {
+	    LinkedHashMap<String, ArrayList<Integer>> groups = new LinkedHashMap<>();
+	    
+	    for (int i = 0; i < ws.size(); i++) {
+	        char[] wa = ws.get(i).toCharArray();
+	        Arrays.sort(wa);
+	        String str = new String(wa);
+	        
+	        if (groups.containsKey(str)) {
+	            groups.get(str).add(i + 1);    
+	        } else {
+	            ArrayList<Integer> pos = new ArrayList<>();
+	            pos.add(i + 1);
+	            groups.put(str, pos);
+	        }
+	    }
+	    
+	    Collection<ArrayList<Integer>> vs = groups.values();
+	    ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+	    for (ArrayList<Integer> v : vs) 
+	        res.add(v);
+	        
+	    return res;
+	}
+}
