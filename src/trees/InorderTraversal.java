9ed6132eacd6ce23bb0a5ffdb7dad88f5f7f9784
Ilya Ismagilov <ismagilov@gmail.com>
2017-10-20 13:15:26 +0700

    Solved without recursion

---
+++ src/trees/InorderTraversal.java
@@ -0,0 +1,33 @@
+package trees;
+
+/**
+ * https://www.interviewbit.com/problems/inorder-traversal/
+ *
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class InorderTraversal {
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+	    final ArrayDeque<TreeNode> stack = new ArrayDeque<>();
+	    final ArrayList<Integer> order = new ArrayList<>();
+	    
+	    TreeNode cur = root; 
+	    while (cur != null || !stack.isEmpty()) {
+	        if (cur != null) {
+	            stack.push(cur);
+	            cur = cur.left;
+	        } else {
+	            cur = stack.pop();
+	            order.add(cur.val);    
+	            cur = cur.right;
+	        }
+	    }
+
+	    return order;
+	}
+}
