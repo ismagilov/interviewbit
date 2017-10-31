ff92d24228f33fd6bcad896813267323d7a7e555
Ilya Ismagilov <ismagilov@gmail.com>
2017-10-31 20:34:58 +0700

    Solved

---
+++ src/stacks_and_queues/RainWaterTrappedB.java
@@ -0,0 +1,27 @@
+/**
+* https://www.interviewbit.com/problems/rain-water-trapped/
+*
+* Very straightforward three-pass solution.
+*/
+public class RainWaterTrappedB {
+	// DO NOT MODIFY THE LIST
+	public int trap(final List<Integer> a) {
+	    int sz = a.size();
+	    int[] leftMaxs = new int[sz];
+	    int[] rightMaxs = new int[sz];
+
+	    leftMaxs[0] = a.get(0);
+	    for (int i = 1; i < a.size(); i++)
+	        leftMaxs[i] = Math.max(a.get(i), leftMaxs[i - 1]);
+	    
+	    rightMaxs[a.size() - 1] = a.get(a.size() - 1);    
+	    for (int i = a.size() - 2; i >= 0; i--)
+	        rightMaxs[i] = Math.max(a.get(i), rightMaxs[i + 1]);
+	    
+	    int v = 0;
+	    for (int i = 0; i < a.size(); i++)
+	        v += Math.min(leftMaxs[i], rightMaxs[i]) - a.get(i);
+	    
+	    return v;
+	}
+}
