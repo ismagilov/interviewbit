58dad0a0fda575c5ea8cef4f6798f3f211e01e2e
Ilya Ismagilov <ismagilov@gmail.com>
2017-10-31 20:14:59 +0700

    Solved

---
+++ src/stacks_and_queues/RainWaterTrappedA.java
@@ -0,0 +1,39 @@
+package stacks_and_queues;
+
+/**
+* https://www.interviewbit.com/problems/rain-water-trapped/
+*
+* One pass solution.
+*/
+public class RainWaterTrappedA {
+	// DO NOT MODIFY THE LIST
+	public int trap(final List<Integer> a) {
+	    if (a.size() <= 2)
+	        return 0;
+	    
+	    int left = 0; 
+	    int right = a.size() - 1;
+	    int leftMax = a.get(left);
+	    int rightMax = a.get(right);
+	    
+	    int v = 0;
+	    while (left < right) {
+	        if (a.get(left) > leftMax)
+	            leftMax = a.get(left);
+	            
+	        if (a.get(right) > rightMax)
+	            rightMax = a.get(right);
+	        
+	        if (leftMax >= rightMax) {
+	            v += rightMax - a.get(right);
+	            right--;
+	        } else {
+	            v += leftMax - a.get(left);
+	            left++;
+	        }
+	    }
+	    
+	    return v;
+	}
+}
+
