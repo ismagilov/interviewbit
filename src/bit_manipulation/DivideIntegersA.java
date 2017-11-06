4f803d28ec0fec0e21c0b7fc4fb3f2f6030a1fc5
Ilya Ismagilov <ismagilov@gmail.com>
2017-11-06 21:34:43 +0700

    Solved

--- src/bit_manipulation/DivideIntegersA.java
+++ src/bit_manipulation/DivideIntegersA.java
@@ -12,23 +12,23 @@ public class DivideIntegersA {
         if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
             sign = -1;
             
-	    long dvd = dividend;
-	    long dvs = divisor;
-	    
-	    dvd = Math.abs(dvd);
-	    dvs = Math.abs(dvs);
-	    
-	    long q = 0;
-	    while (dvd >= dvs) {
-	        long temp = dvs, multiply = 1;
-	        while (dvd >= temp << 1) {
-	            temp <<= 1;
-	            multiply <<= 1;
-	        }
-	        
-	        q += multiply;
-	        dvd -= temp;
-	    }
+        long dvd = dividend;
+        long dvs = divisor;
+
+        dvd = Math.abs(dvd);
+        dvs = Math.abs(dvs);
+
+        long q = 0;
+        while (dvd >= dvs) {
+            long temp = dvs, multiply = 1;
+            while (dvd >= temp << 1) {
+                temp <<= 1;
+                multiply <<= 1;
+            }
+
+            q += multiply;
+            dvd -= temp;
+        }
 
         return (int)(sign == 1 ? q : -q);
 	}
