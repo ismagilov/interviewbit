fd120ace11eda68d83fe537531ac34c95dfcfcd0
Ilya Ismagilov <ismagilov@gmail.com>
2017-11-06 21:33:55 +0700

    Solved

--- src/bit_manipulation/DivideIntegersA.java
+++ src/bit_manipulation/DivideIntegersA.java
@@ -5,43 +5,31 @@ package bit_manipulation;
 */
 public class DivideIntegersA {
 	public int divide(int dividend, int divisor) {
-	    if (divisor == 0)
+	    if (divisor == 0 || (dividend == Integer.MIN_VALUE && divisor == -1))
 	        return Integer.MAX_VALUE;
-        if (dividend == Integer.MIN_VALUE && divisor == -1)
-            return Integer.MAX_VALUE;
-        if (divisor == 1)
-            return dividend;
-        if (divisor == -1)
-            return -dividend;
+
+        int sign = 1;
+        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
+            sign = -1;
             
-	    // d1 / d2 = q
-	    long d1 = dividend;
-	    long d2 = divisor;
-	    long q = 1;
-
-	    int sign = 1;
-	    if (d1 < 0) {
-	        sign = -1;
-	        d1 = -d1;
-	    }
-	    if (d2 < 0) {
-	        sign *= -1;
-	        d2 = -d2;
-	    }
-
-	    if (d2 > d1)
-	        return 0;
-
-	    long tempDivisor = d2;
-	    while ((tempDivisor << 1) < d1) {
-	        tempDivisor <<= 1;
-	        q <<= 1;
-	    }
-	    while (tempDivisor + d2 <= d1) {
-	        tempDivisor += d2;
-	        q += 1;
+	    long dvd = dividend;
+	    long dvs = divisor;
+	    
+	    dvd = Math.abs(dvd);
+	    dvs = Math.abs(dvs);
+	    
+	    long q = 0;
+	    while (dvd >= dvs) {
+	        long temp = dvs, multiply = 1;
+	        while (dvd >= temp << 1) {
+	            temp <<= 1;
+	            multiply <<= 1;
+	        }
+	        
+	        q += multiply;
+	        dvd -= temp;
 	    }
 
-        return (int)(q * sign);
+        return (int)(sign == 1 ? q : -q);
 	}
 }
